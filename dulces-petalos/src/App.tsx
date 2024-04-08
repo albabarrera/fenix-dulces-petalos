867f972da402fc4b750dc17162e14b6695c497a7
albabarrera <alba.barrera@biko2.com>
2024-04-08 13:47:18 +0200

    feat: creates cards and modal window

--- dulces-petalos/src/App.tsx
+++ dulces-petalos/src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './App.css'
+import { Modal } from './components/Modal';
 
-export const App:React.FC = () => {
+const App:React.FC = () => {
+  const [isModalActive, setIsModalActive] = useState(false);
+
   return (
     <>
-      <h1>Vite + React</h1>
+    <main>
+      {isModalActive && (
+        <Modal 
+        onClose={() => setIsModalActive(false)}
+          title="Nombre flor"
+         >
+          <p>Ficha flor</p>
+         </Modal>
+      )}
+      <h1>Dulces Pétalos</h1>
+        <div>
+        <ul className='cardsList'>
+          <li className='card' onClick={() => setIsModalActive(true)}>
+            <h2>Flor 1</h2>
+          </li>
+          <li className='card' onClick={() => setIsModalActive(true)}>
+            <h2>Flor 2</h2>
+          </li>
+          <li className='card' onClick={() => setIsModalActive(true)}>
+            <h2>Flor 3</h2>
+          </li>
+        </ul>
+      </div>
+    </main>
     </>
   )
 }
