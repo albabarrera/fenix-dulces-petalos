2015de2ca573d8b4c1fcdeeb2a2b5fe8857f4692
albabarrera <alba.barrera@biko2.com>
2024-04-08 18:22:07 +0200

    refactor: updates modal window

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import './App.css'
-import { Modal } from './components/Modal';
+import "./App.css";
+import { Modal } from "./components/Modal";
 
-interface Data {
+export interface Data {
   id: string;
   name: string;
   binomialName: string;
@@ -14,60 +14,74 @@ interface Data {
   heightInCm: number;
 }
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   const [isModalActive, setIsModalActive] = useState(false);
-  const [data, setData] = useState<Data[]| null>(null);
+  const [productData, setProductData] = useState<Data | null>(null);
+  const [data, setData] = useState<Data[] | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://dulces-petalos.herokuapp.com/api/product');
-        
+        const response = await fetch(
+          "https://dulces-petalos.herokuapp.com/api/product"
+        );
+
         if (response.ok) {
           const jsonData = await response.json();
 
           setData(jsonData);
         } else {
-          console.error('Error al obtener los datos:', response.statusText);
+          console.error("Error al obtener los datos:", response.statusText);
         }
       } catch (error) {
-        console.error('Error en la llamada a la API:', error);
+        console.error("Error en la llamada a la API:", error);
       }
     };
 
     fetchData();
   }, []);
 
-
   return (
     <>
-    <main>
-      {isModalActive && (
-        <Modal 
-        onClose={() => setIsModalActive(false)}
-          title="Nombre flor"
-         >
-          <p>Ficha flor</p>
-         </Modal>
-      )}
-      <h1>Dulces Pétalos</h1>
+      <main>
+        {isModalActive && productData && (
+          <Modal
+            onClose={() => {
+              setIsModalActive(false);
+              setProductData(null);
+            }}
+            content={productData}
+          />
+        )}
+        <h1>Dulces Pétalos</h1>
         <div>
-        <ul className='cardsList'>
-        {data ? (
-        data.map((product: Data) => 
-          <li key={product.id} className='card' onClick={() => setIsModalActive(true)}>
-            <img src={product.imgUrl} />
-            <h2>{product.name}</h2>
-          </li>
-        )
-      ) : (
-        <p>Cargando datos...</p>
-      )}
-        </ul>
-      </div>
-    </main>
+          <ul className="cardsList">
+            {data ? (
+              data.map((product: Data) => (
+                <>
+                  <li
+                    key={product.id}
+                    className="card"
+                    onClick={() => {
+                      setIsModalActive(true);
+                      setProductData(product);
+                    }}
+                  >
+                    <div className="imageWrapper">
+                      <img className="image" src={product.imgUrl} />
+                    </div>
+                    <h2>{product.name}</h2>
+                  </li>
+                </>
+              ))
+            ) : (
+              <p>Cargando datos...</p>
+            )}
+          </ul>
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
