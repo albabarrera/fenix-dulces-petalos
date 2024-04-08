81df931d3bd74499d463ba1c9d32256cfdf6c5cb
albabarrera <alba.barrera@biko2.com>
2024-04-08 16:32:02 +0200

    feat: gets data from API

--- dulces-petalos/src/App.tsx
+++ dulces-petalos/src/App.tsx
@@ -1,10 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './App.css'
 import { Modal } from './components/Modal';
 
+interface Data {
+  id: string;
+  name: string;
+  binomialName: string;
+  price: number;
+  imgUrl: string;
+  wateringsPerWeek: number;
+  fertilizerType: string;
+  heightInCm: number;
+}
+
 const App:React.FC = () => {
   const [isModalActive, setIsModalActive] = useState(false);
+  const [data, setData] = useState<Data[]| null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dulces-petalos.herokuapp.com/api/product');
+        
+        if (response.ok) {
+          const jsonData = await response.json();
+
+          setData(jsonData);
+        } else {
+          console.error('Error al obtener los datos:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error en la llamada a la API:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
 
   return (
     <>
@@ -20,15 +53,16 @@ const App:React.FC = () => {
       <h1>Dulces Pétalos</h1>
         <div>
         <ul className='cardsList'>
-          <li className='card' onClick={() => setIsModalActive(true)}>
-            <h2>Flor 1</h2>
+        {data ? (
+        data.map((product: Data) => 
+          <li key={product.id} className='card' onClick={() => setIsModalActive(true)}>
+            <img src={product.imgUrl} />
+            <h2>{product.name}</h2>
           </li>
-          <li className='card' onClick={() => setIsModalActive(true)}>
-            <h2>Flor 2</h2>
-          </li>
-          <li className='card' onClick={() => setIsModalActive(true)}>
-            <h2>Flor 3</h2>
-          </li>
+        )
+      ) : (
+        <p>Cargando datos...</p>
+      )}
         </ul>
       </div>
     </main>
