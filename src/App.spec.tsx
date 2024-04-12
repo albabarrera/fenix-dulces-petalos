8e462255fb9ecf6d9690c7182a3fb0f2a5be340c
albabarrera <alba.barrera@biko2.com>
2024-04-12 12:10:27 +0200

    test: tests api success scenario on load

---
+++ src/App.spec.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import { App } from './App';
+import { MemoryRouter } from 'react-router-dom';
+
+
+describe("App", () => {
+    it("api success scenario on load", async () => {
+        render(<MemoryRouter><App /></MemoryRouter>);
+        expect(await screen.findByText(/Girasol/i)).toBeInTheDocument();
+    });
+})
