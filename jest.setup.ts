7d93d4a7a21a034bf4a60f9956b96a69cd0d9cc0
albabarrera <alba.barrera@biko2.com>
2024-04-12 09:21:49 +0200

    chore: installs jest and react testing library

---
+++ jest.setup.ts
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+
+export default {
+    testEnvironment: "jsdom",
+    transform: {
+      "^.+\\.tsx?$": "ts-jest",
+    },
+  
+    moduleNameMapper: {
+      "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+      "^.+\\.svg$": "jest-transformer-svg",
+    },
+  
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  };
